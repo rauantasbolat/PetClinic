c217de2d791738a0e2a32a291867db037b5e04bf
rauantasbolat <rauan98ktl@gmail.com>
2023-03-13 17:15:36 +0600

    commit first my

---
+++ src/main/java/com/rauan/tasbolat/PetClinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.rauan.tasbolat.PetClinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/rauan/tasbolat/PetClinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.rauan.tasbolat.PetClinic.model;
+
+public class Vet extends Person {
+}
